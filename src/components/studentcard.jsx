a3c1c1c37bdbed127171934fc7bdc9d580d87e5a
Aklavya yadav <aklavyayadav0@gmail.com>
2026-08-03 14:46:43 +0530

    Studcard.jsx

---
+++ src/components/studentcard.jsx
@@ -0,0 +1,15 @@
+   function StudentCard() {
+  return (
+    <div className="card">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Student"
+      />
+
+      <h2>Aklavya yadav</h2>
+
+      <p><b>Roll No:</b> 2457074</p>
+
+      <p><b>Branch:</b> CSE</p>
+
+    
