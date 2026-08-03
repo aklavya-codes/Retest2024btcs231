321d04ff325968398d0a24e8b782ecc9c89633d5
Aklavya yadav <aklavyayadav0@gmail.com>
2026-08-03 14:55:29 +0530

    studcardcompleted

--- src/components/studentcard.jsx
+++ src/components/studentcard.jsx
@@ -12,4 +12,11 @@
 
       <p><b>Branch:</b> CSE</p>
 
-    
+      <p><b>Semester:</b> 5th</p>
+
+      <p><b>College:</b> Swami vivekanand institute of engineering and technlogy</p>
+    </div>
+  );
+}
+
+export default StudentCard; 
